32d3d1b08659c58dc65a929dfb249bf322409293
Vita Filatova <filatova.vitta@gmail.com>
2023-04-11 00:55:42 +0300

    внесла правки

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,12 +1,12 @@
 const Joi = require("joi");
-const addContactSchema = Joi.object({
+const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   phone: Joi.string().min(6).required(),
 });
-const updateContactSchema = Joi.object({
+const updateSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string().email(),
   phone: Joi.string().min(6),
 });
-module.exports = { addContactSchema, updateContactSchema };
+module.exports = { addSchema, updateSchema };
